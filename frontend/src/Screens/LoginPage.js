6d236aec01ce737568b35c56b9a64bd791593904
Infinite-Null <ankit.kum.sha9933@gmail.com>
2023-06-08 15:11:58 +0530

    Redesigned the login and signup page

--- frontend/src/Screens/LoginPage.js
+++ frontend/src/Screens/LoginPage.js
@@ -6,7 +6,7 @@ import UserContext from '../Context/User/UserContext'
 export default function LoginPage(){
     const [loading,setloading]=useState(false)
     const navigator=useNavigate()
-    function login(){
+   async  function login(){
         var email=document.querySelector("#exampleFormControlInput1").value
         var password=document.querySelector("#exampleFormControlInput2").value
         if(email==""){
@@ -17,7 +17,7 @@ export default function LoginPage(){
             alert("Password field can't be empty")
         }
         setloading(true)
-        user.login(email,password)
+        await user.login(email,password)
         setloading(false)
         navigator("/")
     }
@@ -28,46 +28,51 @@ export default function LoginPage(){
         alignItems:"center",
         justifyContent:"center",
         height:"100vh",
-        backgroundImage:`url(${background})`,
-        backgroundRepeat:"no-repeat",
-        backgroundSize:"cover",
-        // filter:"brightness(0.85)"
-    }}>
+        // backgroundImage:`url(${background})`,
+        // backgroundRepeat:"no-repeat",
+        // backgroundSize:"cover",
+        // filter:"brightness(0.85)"s
+    }} className='mainDiv'>
     <div style={{
      height:"fit-content",
      display:"inline",
     backdropFilter:"blur(10px)",
-    border:"2px solid white",
-    backgroundColor:"rgba(0,0,0,0.4)",
+    border:"2px solid black",
+    backgroundColor:"rgb(255,255,255)",
      padding:"30px",
-     color:"white",
+     color:"black",
      borderRadius:"10px",
      width:"30rem",
      margin:"10px",
-     backdropFilter:"grayscale(0.6) blur(7px)",
-     boxShadow:"2px 5px 35px 0px rgba(0,0,0,0.5)"
-    }}>
-    <h1 className="display-6 displayGradient">Always Have Track</h1>
-    <h1 className="display-6 displayGradient" style={{
+     boxShadow:"12px 15px 25px 5px rgba(0,0,0,0.4)"
+    }} className='mainDiv1'>
+    <h1 className="display-6 displayGradient mainDiv2">"Always Have Track</h1>
+    <h1 className="display-6 displayGradient mainDiv2" style={{
     fontSize:"1.5em",
     marginBottom:"30px"
-   }}>Of What To Do</h1>
+   }}>Of What To Do"</h1>
    <div className="mb-3">
-  <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
-  <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+  <label htmlFor="exampleFormControlInput1" className="form-label" >Email address</label>
+  <input style={{
+    borderBottom:"2px solid rgba(0,0,0,0.7)"
+  }} type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
    </div>
    <div className="mb-3">
   <label htmlFor="exampleFormControlInput2" className="form-label">Password</label>
-  <input type="password" className="form-control" id="exampleFormControlInput2"/>
+  <input style={{
+    borderBottom:"2px solid rgba(0,0,0,0.7)"
+  }} type="password" className="form-control" id="exampleFormControlInput2"/>
    </div>
    <button className="btn btn-primary" onClick={login}>{loading?(<div className="spinner-border" role="status">
 </div>):"Login"}</button>
    <h1 className="display-6" style={{
     fontSize:"1.5em",
-    margin:"10px"
-   }}>Not a user?  <Link to='/signup'
+    margin:"10px",
+    fontWeight:"400"
+   }}>Not a user? <Link to='/signup'
    style={{
-    color:"lightblue"
+    color:"blue",
+    textDecoration:"none"
    }}>Sign Up</Link></h1>
     </div>
     </div>
--- frontend/src/Screens/Signup.js
+++ frontend/src/Screens/Signup.js
@@ -29,56 +29,63 @@ export default function Signup(){
         alignItems:"center",
         justifyContent:"center",
         height:"100vh",
-        backgroundImage:`url(${background})`,
-        backgroundRepeat:"no-repeat",
-        backgroundSize:"cover",
         // filter:"brightness(0.85)"
-    }}>
+    }} className='mainDiv'>
     <div style={{
      height:"fit-content",
      display:"inline",
     backdropFilter:"blur(10px)",
-    border:"2px solid white",
-    backgroundColor:"rgba(0,0,0,0.4)",
+    border:"2px solid black",
+    backgroundColor:"white",
      padding:"30px",
-     color:"white",
+     color:"black",
      borderRadius:"10px",
      width:"30rem",
      height:"40rem",
      margin:"10px",
      backdropFilter:"grayscale(0.6) blur(7px)",
      boxShadow:"2px 5px 35px 0px rgba(0,0,0,0.5)"
-    }}>
-    <h1 className="display-6 displayGradient">Always Have Track</h1>
-    <h1 className="display-6 displayGradient" style={{
+    }} className='mainDiv1'>
+    <h1 className="display-6 displayGradient mainDiv2">"Always Have Track</h1>
+    <h1 className="display-6 displayGradient mainDiv2" style={{
     fontSize:"1.5em",
     marginBottom:"40px",
-   }}>Of What To Do</h1>
+   }}>Of What To Do"</h1>
    <div className="mb-3">
   <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
-  <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+  <input style={{
+    borderBottom:"2px solid rgba(0,0,0,0.7)"
+  }}  type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
    </div>
    <div className="mb-3">
   <label htmlFor="exampleFormControlInput2" className="form-label">Password</label>
-  <input type="password" className="form-control" id="exampleFormControlInput2"/>
+  <input style={{
+    borderBottom:"2px solid rgba(0,0,0,0.7)"
+  }} type="password" className="form-control" id="exampleFormControlInput2"/>
    </div>
    <div className="mb-3">
   <label htmlFor="exampleFormControlInput3" className="form-label">Frist Name</label>
-  <input type="text" className="form-control" id="exampleFormControlInput3" placeholder=""/>
+  <input style={{
+    borderBottom:"2px solid rgba(0,0,0,0.7)"
+  }} type="text" className="form-control" id="exampleFormControlInput3" placeholder=""/>
    </div>
    <div className="mb-3">
   <label htmlFor="exampleFormControlInput4" className="form-label">Last Name</label>
-  <input type="text" className="form-control" id="exampleFormControlInput4" placeholder=""/>
+  <input style={{
+    borderBottom:"2px solid rgba(0,0,0,0.7)"
+  }} type="text" className="form-control" id="exampleFormControlInput4" placeholder=""/>
    </div>
    <button className="btn btn-primary" onClick={signup}>{loading?(<div className="spinner-border" role="status">
 </div>):"Signup"}</button>
    <h1 className="display-6" style={{
     fontSize:"1.5em",
-    margin:"10px"
+    margin:"10px",
+    fontWeight:"400"
    }}>Already a user?  <Link
    to="/login"
    style={{
-    color:"lightblue"
+    color:"blue",
+    textDecoration:"none"
    }}>Login</Link></h1>
     </div></div>
 }
